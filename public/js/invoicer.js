b7801d9a312cca438a93850957a4d4778e18f2db
djfm <fm.de.jouvencel@gmail.com>
2014-04-03 15:19:28 +0000

    // implemented additional rounding methods

--- public/js/invoicer.js
+++ public/js/invoicer.js
@@ -4,8 +4,27 @@
 
 function roundAmount(amount, method)
 {
-	return Math.round(100*amount)/100;
-};
+	if (method === 'floor')
+	{
+		return Math.floor(100*amount)/100;
+	}
+	else if (method === 'ceiling')
+	{
+		return Math.ceil(100*amount)/100;
+	}
+	else if (method === 'bankers')
+	{
+		var n = (amount * 100).toFixed(8); // Avoid rounding errors
+		var i = Math.floor(n), f = n - i;
+		var e = 1e-8; // Allow for rounding errors in f
+		var r = (f > 0.5 - e && f < 0.5 + e) ? ((i % 2 === 0) ? i : i + 1) : Math.round(n);
+		return r / 100;
+	}
+	else // default: half_up
+	{
+		return Math.round(100*amount)/100;
+	}
+}
 
 /*
 Compute an invoice.
